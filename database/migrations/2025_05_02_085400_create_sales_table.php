696ae2867b74a4330309d91be36e0a8fd065f0aa
rama47 <ramantulwe@gmail.com>
2025-05-03 08:16:44 +0300

    creating data table for mysql database and migrate

---
+++ database/migrations/2025_05_02_085400_create_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('receipt_code')->unique(); // Unique receipt code for each sale
+            $table->decimal('total_amount', 10, 2);
+            $table->foreignId('sales_status')->nullable()->constrained('statuses')->onDelete('set null');
+            $table->foreignId('payment_method')->nullable()->constrained('payment_methods')->onDelete('set null');
+            $table->foreignId('payment_status')->nullable()->constrained('statuses')->onDelete('set null');
+            $table->text('notes')->nullable(); // Optional notes for the sale
+            $table->timestamp('sale_date')->useCurrent();
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
